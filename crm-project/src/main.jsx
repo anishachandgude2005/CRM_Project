ba7c6f4bfd3d100065aa0736aa4635df4e55e5c1
gaikwadisha95-droid <gaikwadisha95@gmail.com>
2026-02-08 20:35:24 +0530

    change AppContext

--- crm-project/src/main.jsx
+++ crm-project/src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { AppProvider } from "./context/AppContext";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
