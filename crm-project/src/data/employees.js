b40d5cd9aae01e6b57a5942d9189f01f49a84065
gaikwadisha95-droid <gaikwadisha95@gmail.com>
2026-02-11 11:01:16 +0530

    employee mock changes

--- crm-project/src/data/employees.js
+++ crm-project/src/data/employees.js
@@ -1,4 +1,23 @@
 export const employees = [
-  { id: 1, name: "Vikram Sales", role: "Sales" },
-  { id: 2, name: "Pooja Support", role: "Support" }
+  {
+    id: 1,
+    name: "Anisha Chandgude",
+    email: "anishachandgude2005@gmail.com",
+    role: "Employee",
+    active: true
+  },
+  {
+    id: 2,
+    name: "Vikram Sales",
+    email: "vikram@sales.com",
+    role: "Sales",
+    active: false
+  },
+  {
+    id: 3,
+    name: "Pooja Support",
+    email: "pooja@support.com",
+    role: "Support",
+    active: true
+  }
 ];
