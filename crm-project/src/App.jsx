372399cb31b203b0ee22ef9ffebc954e2f15c4ce
pratikshabhosale2145-droid <pratikshabhosale2145-droid>
2026-03-01 20:19:53 +0530

    deleted vita icon

--- crm-project/src/App.jsx
+++ crm-project/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
