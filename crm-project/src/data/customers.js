e960df44c7342c8676715bfe6842c1297cab1bbe
gaikwadisha95-droid <gaikwadisha95@gmail.com>
2026-04-03 09:57:40 +0530

    change

--- crm-project/src/data/customers.js
+++ crm-project/src/data/customers.js
@@ -1,4 +1,4 @@
 export const customers = [
-  { id: 1, name: "Neha Singh", email: "neha@gmail.com", phone: "9022539490",  company: "TATA", Status:"New"},
-  { id: 2, name: "Rohit Mehta", email: "rohit@gmail.com", phone: "9970306357", company: "TATA", status:"New" }
+  { id: 1, name: "Neha Singh", email: "neha@gmail.com", phone: "9022539490",  company: "TATA", Status:"Active"},
+  { id: 2, name: "Rohit Mehta", email: "rohit@gmail.com", phone: "9970306357", company: "TATA", status:"Acive" }
 ];
