da3ee59686038bdd28176f952d78e258824d609d
gaikwadisha95-droid <gaikwadisha95@gmail.com>
2026-04-03 09:51:32 +0530

    appContext update

--- crm-project/src/data/tasks.js
+++ crm-project/src/data/tasks.js
@@ -1,16 +1,24 @@
 export const tasks = [
-  {
-    id: 1,
-    title: "Call New Lead",
-    assignedBy: "Sales User",
-    dueDate: "2026-02-15",
-    completed: false
-  },
-  {
-    id: 2,
-    title: "Send Proposal",
-    assignedBy: "Admin",
-    dueDate: "2026-02-12",
-    completed: true
-  }
+  
+    {
+      id: 1,
+      title: "Call New Lead",
+      description: "Call the customer and explain the product details.",
+      assignedBy: "Sales User",
+      assignedTo: "Rohit",
+      dueDate: "2026-02-15",
+      priority: "High",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Send Proposal",
+      description: "Send pricing and package proposal to customer.",
+      assignedBy: "Admin",
+      assignedTo: "Neha",
+      dueDate: "2026-02-12",
+      priority: "Medium",
+      completed: true
+    }
+  
 ];
