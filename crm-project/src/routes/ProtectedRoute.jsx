db5cddb3bc442041248189b51cda2f6fca913dfc
radha0202 <91457900+Shubham0202@users.noreply.github.com>
2026-04-05 12:17:31 +0530

    fix protected route issue

--- crm-project/src/routes/ProtectedRoute.jsx
+++ crm-project/src/routes/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const user = JSON.parse(localStorage.getItem("currentUser"));
+  const user = JSON.parse(localStorage.getItem("crmUser"));
 
-  console.log("ProtectedRoute user:", user); // ✅ DEBUG
+  // console.log("ProtectedRoute user:", user); // ✅ DEBUG
 
   if (!user) {
     return <Navigate to="/login" />;
